1b993acd3ff43642931cdc60b2d43b43739502e1
Huseynov55 <anarchh-apa202@code.edu.az>
2026-04-10 14:07:09 +0400

    CourseApp

---
+++ 15-ConsoleApp/DomainLayer/Entities/CourseGroup.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Entities
+{
+    public class CourseGroup : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public int Room { get; set; }
+    }
+}
---
+++ 15-ConsoleApp/RepositoryLayer/Repositories/Interfaces/IGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DomainLayer.Entities;
+namespace RepositoryLayer.Repositories.Interfaces
+{
+    public interface IGroupRepository : IRepository<CourseGroup>
+    {
+        List<CourseGroup> GetAllByTeacher(string teacher);
+        List<CourseGroup> GetAllByRoom(string room);
+        List<CourseGroup> SearchByName(string name);
+
+       
+    }
+}
